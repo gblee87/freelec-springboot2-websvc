71623d575aeba88d54ef8dc7ba1fc074e604a89b
gblee87 <pechando@gmail.com>
2020-02-20 14:40:40 +0900

    20200220 # p144 게시글 등록 - 게시글 등록시 HTTP Method POST가 등록되지 않는다고 나옴 - @PostMapping 어노테이션이 적용 안되는듯 한데 어디를 봐야할지 조금 난감함

---
+++ src/main/java/com/study/admin/service/PostsService.java
@@ -0,0 +1,59 @@
+package com.study.admin.service;
+
+import com.study.admin.domain.posts.Posts;
+import com.study.admin.domain.posts.PostsRepository;
+import com.study.admin.web.dto.PostsListResponseDto;
+import com.study.admin.web.dto.PostsResponseDto;
+import com.study.admin.web.dto.PostsSaveRequestDto;
+import com.study.admin.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @org.springframework.transaction.annotation.Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @org.springframework.transaction.annotation.Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+
+        return id;
+    }
+
+    @org.springframework.transaction.annotation.Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        postsRepository.delete(posts);
+    }
+
+    @org.springframework.transaction.annotation.Transactional(readOnly = true)
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
+
+        return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/study/admin/web/IndexController.java
@@ -0,0 +1,19 @@
+package com.study.admin.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+        System.out.println("posts save 탄다.");
+        return "posts-save";
+    }
+}
